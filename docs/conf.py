8d7c2507fb92826d34bc22e46bbe13e538268a01
Glenn Matthews <glenn@e-dad.net>
2015-02-19 14:02:47 -0500

    Version string fixup for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -73,15 +73,23 @@ copyright = u'2013-2015, the COT project developers'
 # built documents.
 #
 from COT._version import get_versions, run_command   # noqa
-# The full version, including alpha/beta/rc tags.
-release = get_versions()["version"]
-# The short X.Y version.
-version = release.split("+")[0]
 
-if re.search("dirty", release):
-    print(run_command(["git"], ["describe", "--tags", "--dirty",
-                                "--always", "--long"]))
-    print(run_command(["git"], ["diff"]))
+versioneer_string = get_versions()["version"]
+# readthedocs patches conf.py, causing git to report the version as 'dirty',
+# and hence causing versioneer to do likewise. Let's clean that up.
+if re.search(".dirty$", versioneer_string):
+    versioneer_string = versioneer_string[:-6]
+# Get the tagged version number:
+release = versioneer_string.split("+")[0]
+# If the commit count is 0 we are on a tagged version and the above
+# suffices. If it's greater than zero, let's append that
+post0 = re.search("\+([1-9][0-9]*)", versioneer_string)
+if post0:
+    # We are past a tagged version by +N commits
+    commit_count = post0.group(1)
+    release += " (plus {0} commits)".format(commit_count)
+
+version = release
 
 print("\n".join([project, version, release]))
 
