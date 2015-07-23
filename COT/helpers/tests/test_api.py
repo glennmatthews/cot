438c00b16d6e2f5ff9559b50204bead314681a6b
Quol Fontana <quol.fontana@gmail.com>
2015-07-23 08:26:33 -0500

    enable product & vendor production section fields

--- COT/helpers/tests/test_api.py
+++ COT/helpers/tests/test_api.py
@@ -38,7 +38,7 @@ class TestGetChecksum(COT_UT):
     def test_get_checksum_md5(self):
         """Test case for get_checksum() with md5 sum."""
         checksum = get_checksum(self.input_ovf, 'md5')
-        self.assertEqual(checksum, "5aa4e3defb16e02ea16dd07cff77bfdf")
+        self.assertEqual(checksum, "b77010bab91c4a22b3a2ef26dc5347ff")
 
         checksum = get_checksum(self.minimal_ovf, 'md5')
         self.assertEqual(checksum, "288e1e3fcb05265cd9b8c7578e173fef")
@@ -47,7 +47,7 @@ class TestGetChecksum(COT_UT):
         """Test case for get_checksum() with sha1 sum."""
         checksum = get_checksum(self.input_ovf, 'sha1')
         self.assertEqual(checksum,
-                         "63b6c9d71cc8b051ffbfa8d1d630d30a2dfb9701")
+                         "f6d5fcdab4114d22af3da963aac8506bf3756476")
 
         checksum = get_checksum(self.minimal_ovf, 'sha1')
         self.assertEqual(checksum,
