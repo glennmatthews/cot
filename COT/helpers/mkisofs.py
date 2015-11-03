119bca82d84c53d6877574ccfec48ae8235e91ad
Glenn Matthews <glenn@e-dad.net>
2015-11-03 09:43:05 -0500

    Allow xorriso as ISO helper

--- COT/helpers/mkisofs.py
+++ COT/helpers/mkisofs.py
@@ -14,9 +14,10 @@
 # of COT, including this file, may be copied, modified, propagated, or
 # distributed except according to the terms contained in the LICENSE.txt file.
 
-"""Give COT access to ``mkisofs`` or ``genisoimage`` for creating ISO images.
+"""Give COT access to mkisofs, genisoimage, or xorriso for creating ISO images.
 
 http://cdrecord.org/
+https://www.gnu.org/software/xorriso/
 """
 
 import logging
@@ -28,9 +29,10 @@ logger = logging.getLogger(__name__)
 
 class MkIsoFS(Helper):
 
-    """Helper provider for ``mkisofs`` and/or ``genisoimage``.
+    """Helper provider for ``mkisofs``, ``genisoimage``, or ``xorriso``.
 
     http://cdrecord.org/
+    https://www.gnu.org/software/xorriso/
 
     **Methods**
 
@@ -45,29 +47,33 @@ class MkIsoFS(Helper):
         """Initializer."""
         super(MkIsoFS, self).__init__(
             "mkisofs",
-            version_regexp="(?:mkisofs|genisoimage) ([0-9.]+)")
+            version_regexp="(?:mkisofs|genisoimage|xorriso) ([0-9.]+)")
 
     @property
     def name(self):
-        """Either ``mkisofs`` or ``genisoimage`` depending on environment."""
+        """Either mkisofs, genisoimage, or xorriso depending on environment."""
         if not self._path:
             self._path = self.find_executable("mkisofs")
             if self._path:
                 self._name = "mkisofs"
-            else:
-                self._path = self.find_executable("genisoimage")
-                if self._path:
-                    self._name = "genisoimage"
+        if not self._path:
+            self._path = self.find_executable("genisoimage")
+            if self._path:
+                self._name = "genisoimage"
+        if not self._path:
+            self._path = self.find_executable("xorriso")
+            if self._path:
+                self._name = "xorriso"
         return self._name
 
     @property
     def path(self):
-        """Find either ``mkisofs`` or ``genisoimage`` if available."""
+        """Find ``mkisofs``, ``genisoimage``, or ``xorriso`` if available."""
         self.name
         return self._path
 
     def install_helper(self):
-        """Install ``mkisofs`` and/or ``genisoimage``."""
+        """Install ``mkisofs``, ``genisoimage``, or ``xorriso``."""
         if self.path:
             logger.warning("Tried to install {0} -- "
                            "but it's already available at {1}!"
@@ -79,6 +85,8 @@ class MkIsoFS(Helper):
         elif (Helper.apt_install('genisoimage') or
               Helper.yum_install('genisoimage')):
             self._name = "genisoimage"
+        elif Helper.apt_install('xorriso'):
+            self._name = "xorriso"
         else:
             raise NotImplementedError(
                 "Unsure how to install mkisofs.\n"
@@ -94,7 +102,10 @@ class MkIsoFS(Helper):
         """
         logger.info("Calling {0} to create an ISO image"
                     .format(self.name))
-        # mkisofs and genisoimage take the same parameters, conveniently
-        self.call_helper(['-output', file_path,
-                          '-full-iso9660-filenames',
-                          '-iso-level', '2'] + contents)
+        # mkisofs and genisoimage take the same parameters, conveniently,
+        # while xorriso needs to be asked to pretend to be mkisofs
+        args = ['-output', file_path, '-full-iso9660-filenames',
+                '-iso-level', '2'] + contents
+        if self.name == 'xorriso':
+            args = ['-as', 'mkisofs'] + args
+        self.call_helper(args)
