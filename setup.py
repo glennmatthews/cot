1f40528fd44f36b7ad66db19c317a31f74c90dcc
Glenn Matthews <glenn@e-dad.net>
2016-06-04 20:37:15 -0400

    Update copyright date

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # setup.py - installer script for COT package
 #
 # April 2014, Glenn F. Matthews
-# Copyright (c) 2014-2015 the COT project developers.
+# Copyright (c) 2014-2016 the COT project developers.
 # See the COPYRIGHT.txt file at the top-level directory of this distribution
 # and at https://github.com/glennmatthews/cot/blob/master/COPYRIGHT.txt.
 #
