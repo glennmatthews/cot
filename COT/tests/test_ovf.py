4a812a1368255e2fbfb44fa4aed7122fc6068de5
Glenn Matthews <glenn@e-dad.net>
2016-06-05 10:31:43 -0400

    Copyright date fix

--- COT/tests/test_ovf.py
+++ COT/tests/test_ovf.py
@@ -3,7 +3,7 @@
 # ovf.py - Unit test cases for COT OVF/OVA handling
 #
 # September 2013, Glenn F. Matthews
-# Copyright (c) 2013-2015 the COT project developers.
+# Copyright (c) 2013-2016 the COT project developers.
 # See the COPYRIGHT.txt file at the top-level directory of this distribution
 # and at https://github.com/glennmatthews/cot/blob/master/COPYRIGHT.txt.
 #
