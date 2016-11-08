c7ca1dadf3a1ae1a3b2270d9853cf8c5efd6262a
Glenn Matthews <glenn@e-dad.net>
2016-11-08 11:15:45 -0500

    needs_sphinx doesn't support minor versions in sphinx 1.3.x

--- docs/conf.py
+++ docs/conf.py
@@ -233,7 +233,7 @@ regenerate_usage_contents(force=True)
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.3.1'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
