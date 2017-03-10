e8088021056ae62c0b9704734a42ea23b332e961
Glenn Matthews <glenn@e-dad.net>
2017-03-10 16:16:04 -0500

    Add sanity test case.

--- COT/vm_description/ovf/tests/test_hardware.py
+++ COT/vm_description/ovf/tests/test_hardware.py
@@ -21,6 +21,26 @@ from COT.vm_description.ovf import OVF
 class TestOVFHardware(COT_UT):
     """Unit test cases for the OVFHardware class."""
 
+    def test_basic_sanity(self):
+        """Make sure basic loading is successful and APIs work."""
+        with OVF(self.input_ovf, None) as ovf:
+            hw = ovf.hardware
+            self.assertEqual(ovf, hw.ovf)
+            self.assertEqual(13, len(hw.item_dict))
+            self.assertEqual('14', hw.find_unused_instance_id())
+            self.assertEqual(2, len(hw.find_all_items('ide')))
+            self.assertEqual(1, len(hw.find_all_items(
+                'ide', properties={ovf.ADDRESS: '1'})))
+            self.assertEqual(0, len(hw.find_all_items(
+                'ide', properties={ovf.ADDRESS: '2'})))
+            self.assertNotEqual(None, hw.find_item('harddisk'))
+            self.assertEqual(None, hw.find_item('usb'))
+            self.assertRaises(LookupError, hw.find_item, 'ide')
+            self.assertEqual(1, hw.get_item_count('ethernet', None))
+            self.assertEqual(3, hw.get_item_count('ethernet', "4CPU-4GB-3NIC"))
+            self.assertEqual(3, hw.get_item_count_per_profile(
+                'ethernet', None)["4CPU-4GB-3NIC"])
+
     def test_find_item_multiple_matches(self):
         """Check find_item raises LookupError if multiple matches are found."""
         with OVF(self.input_ovf, None) as ovf:
